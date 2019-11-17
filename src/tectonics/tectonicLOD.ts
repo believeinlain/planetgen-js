7a2d945a06015a1b510542bd733df5087e58a9e2
Stephan Aelmore <saelmore@connectiveunconscious.com>
2019-11-17 06:34:10 +0000

    Finally have working subdividing tectonics.

--- src/tectonics/tectonicLOD.ts
+++ src/tectonics/tectonicLOD.ts
@@ -41,7 +41,7 @@ class FaultLink {
 
 class TectonicLOD extends SphereLOD {
   startingNodeDensity: number; // ratio of faces that should be nodes
-  links: FaultLink[]; // use instanceof to check if a FaultLink is a FaultNode
+  links: Map<Face, FaultLink>; // use faces as index since FaultLinks have a reference to Face already
   rng; // seeded random number generator
   unpickedFaces: Face[]; // all faces that don't have FaultLinks on them
   linkedEdges: Edge[]; // aa edges between two FaultLinks
@@ -54,7 +54,7 @@ class TectonicLOD extends SphereLOD {
   protected _initialize(newRadius: number, pointsRef: Point[]): void {
     super._initialize(newRadius, pointsRef);
     this.rng = seedrandom(315);
-    this.links = new Array<FaultLink>();
+    this.links = new Map<Face, FaultLink>();
     this.startingNodeDensity = 0.15;
   }
 
@@ -78,7 +78,7 @@ class TectonicLOD extends SphereLOD {
       targetEdge.faces.forEach( (face) => {
 	let newLink = new FaultLink(face);
 	newLinks.push(newLink);
-	this.links.push(newLink);
+	this.links.set(face, newLink);
 	// remove the face from unpicked faces
 	this.unpickedFaces.filter(unpickedFace => unpickedFace !== face);
       });
@@ -86,9 +86,32 @@ class TectonicLOD extends SphereLOD {
       this._connectLinks(newLinks[0], newLinks[1]);
     });
 
-    // iterate through all super faces
-    priorLOD.faces.forEach( (face) => {
+    // iterate through all super faces with any links on them
+    priorLOD.links.forEach( (superLink) => {
+      let superFace = superLink.location;
       // bridge between the faultlinks on this super face
+      let linkFaceIndices = new Array<number>();
+      // each index corresponds to a specific subface relative to points and edges as constructed
+      // essentially the index tells us where on the super face each subface is located
+      for (let index=0; index<superFace.subFaces.length; ++index) {
+	// add the index of subFace to linkFaceIndices of it has a link on it
+	if (this.links.has(superFace.subFaces[index])) {
+	  linkFaceIndices.push(index);
+	}
+      }
+
+      // if there are 2 or 3 subfaces with links on them, they must connect through the center
+      if ((linkFaceIndices.length == 2) || (linkFaceIndices.length == 3)) {
+	// create center link
+	let centerLink = new FaultLink(superFace.subFaces[3]); // 3 is the index of the center subFace
+	this.links.set(superFace.subFaces[3], centerLink);
+	// link each of the links on this superFace to the centerLink
+	for (let index of linkFaceIndices) {
+	  this._connectLinks(this.links.get(superFace.subFaces[index]), centerLink);
+	}
+      } else {
+	console.log("Error, each face with a link should have 2 or 3 connections, this on seems to have "+linkFaceIndices.length);
+      }
     });
 
     // update all links
@@ -106,39 +129,42 @@ class TectonicLOD extends SphereLOD {
     this.linkedEdges = new Array<Edge>();
 
     // add starting nodes until we've reached startingNodeDensity
-    while (this.links.length < (this.faces.length * this.startingNodeDensity)) {
+    while (this.links.size < (this.faces.length * this.startingNodeDensity)) {
       // pick a random face
       let pick = Math.floor(this.rng() * this.unpickedFaces.length);
 
       // create a new FaultLink at that face
-      this.links.push(new FaultLink(this.unpickedFaces[pick]));
+      this.links.set(this.unpickedFaces[pick], new FaultLink(this.unpickedFaces[pick]));
       // remove the selected face so we don't pick it again
       this.unpickedFaces.splice(pick, 1);
     }
 
     // iterate through initial nodes and ensure all adjacent faces have links
     this.links.forEach( (link) => {
-      // iterate through adjacent faces
-      link.location.getAdjacentFaces().forEach( (face) => {
-	// find the face in unpickedFaces, if not found, unpickedIndex = -1
-	let unpickedIndex = this.unpickedFaces.indexOf(face);
-	if (unpickedIndex === -1) {
-	  // face has already been picked, so link its node to this node
-	  this.links.forEach( (otherLink) => { if (otherLink.location == face) otherLink.linkTo(link); });
-	} else {
-	  // face hasn't been picked so create a link there and link to it
-	  let newLink = new FaultLink(this.unpickedFaces[unpickedIndex]);
-	  this.links.push(newLink);
-	  this._connectLinks(newLink, link);
-	  // and remove it from unpickedFaces
-	  this.unpickedFaces.splice(unpickedIndex, 1);
-	}
-      });
+      // if initial node, then it will have no connections
+      if (link.connections.size == 0) {
+	// iterate through adjacent faces
+	link.location.getAdjacentFaces().forEach( (face) => {
+	  // find the face in unpickedFaces, if not found, unpickedIndex = -1
+	  let unpickedIndex = this.unpickedFaces.indexOf(face);
+	  if (unpickedIndex === -1) {
+	    // face has already been picked, so link its node to this node
+	    this._connectLinks(this.links.get(face), link);
+	  } else {
+	    // face hasn't been picked so create a link there and link to it
+	    let newLink = new FaultLink(this.unpickedFaces[unpickedIndex]);
+	    this.links.set(this.unpickedFaces[unpickedIndex], newLink);
+	    this._connectLinks(newLink, link);
+	    // and remove it from unpickedFaces
+	    this.unpickedFaces.splice(unpickedIndex, 1);
+	  }
+	});
+      }
     });
     
     // iterate through all links and ensure they all have at least 2 connections
     this._expandAllLinks();
-    
+
     // update all links
     this.links.forEach(link => link.updateDrawType());
   }
@@ -152,16 +178,11 @@ class TectonicLOD extends SphereLOD {
 	this.linkedEdges.push(edge);
       }
     });
-    link1.location.edges.forEach( (edge) => {
-      if (edge.getFaceAdjacentTo(link1.location) == link0.location) {
-	this.linkedEdges.push(edge);
-      }
-    });
   }
 
   // iterate through all links and ensure they all have at least 2 connections
   protected _expandAllLinks(): void {
-    for (let link of this.links) {
+    for (let link of this.links.values()) {
       if (link.connections.size < 2) {
 	// get adjacent links to this link
 	let possibleConnectionFaces = new Set<Face>(link.location.getAdjacentFaces());
@@ -179,7 +200,7 @@ class TectonicLOD extends SphereLOD {
 	} else {
 	  // create a new FaultLink at targetFace
 	  let newLink = new FaultLink(targetFace);
-	  this.links.push(newLink);
+	  this.links.set(targetFace, newLink);
 	  this._connectLinks(newLink, link);
 	  // and remove it from unpickedFaces
 	  this.unpickedFaces.splice(unpickedIndex, 1);
