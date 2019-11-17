ba33134a71ab7c986b7a4538a487fedc65d1cae3
Stephan Aelmore <saelmore@connectiveunconscious.com>
2019-11-17 02:22:29 +0000

    Moved source to ./src/ directory

---
+++ src/main.ts
@@ -0,0 +1,118 @@
+
+import { Planet } from './planet';
+
+import * as BABYLON from 'babylonjs';
+import * as GUI from 'babylonjs-gui';
+
+class Game {
+  private _canvas: HTMLCanvasElement;
+  private _engine: BABYLON.Engine;
+  private _scene: BABYLON.Scene;
+  private _camera: BABYLON.ArcRotateCamera;
+  private _light: BABYLON.Light;
+  private _gui: GUI.AdvancedDynamicTexture;
+
+  private _planet: Planet;
+  private _icoMesh: BABYLON.Mesh;
+  private _icoMaterial: BABYLON.StandardMaterial;
+
+  constructor(canvasElement: string) {
+    // Create canvas and engine.
+    this._canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
+    this._engine = new BABYLON.Engine(this._canvas, true);
+  }
+
+  createScene(): void {
+    // Create a basic BJS Scene object.
+    this._scene = new BABYLON.Scene(this._engine);
+
+    // Create a camera, and set its position
+    this._camera = new BABYLON.ArcRotateCamera('camera', 0, 0, 10, new BABYLON.Vector3(0, 0, 0), this._scene);
+
+    // Target the camera to scene origin.
+    this._camera.setTarget(BABYLON.Vector3.Zero());
+
+    // Attach the camera to the canvas.
+    this._camera.attachControl(this._canvas, false);
+
+    // Create a basic light, aiming 0,1,0 - meaning, to the sky.
+    this._light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), this._scene);
+
+    // create the GUI
+    this._gui = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+
+    var panel = new GUI.StackPanel();
+    panel.width = "220px";
+    panel.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+    panel.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_CENTER;
+    this._gui.addControl(panel);
+
+    var header = new GUI.TextBlock();
+    header.text = "LOD Level: 0";
+    header.height = "30px";
+    header.color = "black";
+    panel.addControl(header); 
+
+    var slider = new GUI.Slider();
+    slider.minimum = 0;
+    slider.maximum = 6;
+    slider.value = 0;
+    slider.step = 1;
+    slider.height = "20px";
+    slider.width = "200px";
+    slider.onValueChangedObservable.add( (value) => {
+      header.text = "LOD Level: " + value;
+      this.updatePlanet(value);
+    });
+    panel.addControl(slider); 
+
+    // make an icosphere as our 'planet'
+    this._planet = new Planet(2, 0);
+    this._icoMesh = new BABYLON.Mesh('ico', this._scene);
+
+    // use debug texture for this thing
+    this._icoMaterial = new BABYLON.StandardMaterial("mat", this._scene);
+    this._icoMaterial.diffuseTexture = new BABYLON.Texture("../planetgen/PlanetgenDebug_color.png", this._scene);
+    this._icoMesh.material = this._icoMaterial;
+
+    // initialize _icoMesh
+    this.updatePlanet();
+  }
+
+  doRender(): void {
+    // Run the render loop.
+    this._engine.runRenderLoop(() => {
+      this._scene.render();
+    });
+
+    // The canvas/window resize event handler.
+    window.addEventListener('resize', () => {
+      this._engine.resize();
+    });
+  }
+
+  // redraw _icosphere at the specified LOD
+  updatePlanet(newLOD: number = 0): void {
+    // update the mesh once the promise fulfills
+    this._planet.getUpdatedLODMeshAsync(newLOD).then( (result) => {
+      let vertexData = new BABYLON.VertexData();
+
+      vertexData.positions = result.vertices;
+      vertexData.indices = result.indices;
+      vertexData.uvs = result.uvs;
+
+      vertexData.applyToMesh(this._icoMesh);
+    });
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  // Create the game using the 'renderCanvas'.
+  let game = new Game('renderCanvas');
+
+  // Create the scene.
+  game.createScene();
+
+  // Start render loop.
+  game.doRender();
+});
---
+++ src/tectonics/tectonicLOD.ts
@@ -0,0 +1,192 @@
+
+import { Point, Edge, Face, DrawType } from '../geometry/primitives';
+import { SphereLOD } from '../geometry/sphereLOD';
+
+// allow for seeded rng
+var seedrandom = require('seedrandom');
+
+// links between two other FaultLinks to form a faultline
+class FaultLink {
+  location: Face;
+  connections: Set<FaultLink>;
+
+  constructor (newLocation: Face) {
+    this.location = newLocation;
+    this.connections = new Set<FaultLink>();
+  }
+
+  // update the DrawType on this.location
+  updateDrawType(): void {
+    // set drawtype based on number of connections
+    switch(this.connections.size) {
+      case 3:
+	this.location.drawType = DrawType.DebugNode;
+	break;
+      case 2:
+	this.location.drawType = DrawType.DebugLink;
+	break;
+      default:
+	//console.log("Error: FaultLink detected with "+this.connections.size+" connections. Should be 3 or 2.");
+	this.location.drawType = DrawType.DebugLink; // temporarily bypassed for testing
+	//this.location.drawType = DrawType.DebugNormal;
+    }
+  }
+
+  // connect this link to another link
+  linkTo(link: FaultLink): void {
+    this.connections.add(link);
+    link.connections.add(this);
+  }
+};
+
+class TectonicLOD extends SphereLOD {
+  startingNodeDensity: number; // ratio of faces that should be nodes
+  links: FaultLink[]; // use instanceof to check if a FaultLink is a FaultNode
+  rng; // seeded random number generator
+  unpickedFaces: Face[]; // all faces that don't have FaultLinks on them
+  linkedEdges: Edge[]; // aa edges between two FaultLinks
+
+  constructor (newRadius: number, pointsRef: Point[], priorLOD?: TectonicLOD) {
+    super(newRadius, pointsRef, priorLOD);
+    // nothing goes here, we just override protected methods of sphereLOD instead
+  }
+
+  protected _initialize(newRadius: number, pointsRef: Point[]): void {
+    super._initialize(newRadius, pointsRef);
+    this.rng = seedrandom(315);
+    this.links = new Array<FaultLink>();
+    this.startingNodeDensity = 0.15;
+  }
+
+  // called if priorLOD is passed to the constructor
+  // code in this function will be called in the super.constructor prior to mesh generation
+  protected _subdivide(priorLOD: TectonicLOD): void {
+    // subdivide mesh
+    super._subdivide(priorLOD);
+
+    // initialize unpicked faces after subdividing mesh
+    this.unpickedFaces = this.faces.slice();
+    this.linkedEdges = new Array<Edge>();
+
+    // iterate though all super edges that connect two links
+    priorLOD.linkedEdges.forEach( (edge) => {
+      // pick a random edge for the fault to pass through
+      let targetEdge = edge.subEdges[Math.round(this.rng())];
+
+      // create new FaultLinks at that edge
+      let newLinks = new Array<FaultLink>();
+      targetEdge.faces.forEach( (face) => {
+	let newLink = new FaultLink(face);
+	newLinks.push(newLink);
+	this.links.push(newLink);
+	// remove the face from unpicked faces
+	this.unpickedFaces.filter(unpickedFace => unpickedFace !== face);
+      });
+      // link them to each other
+      newLinks[0].linkTo(newLinks[1]);
+    });
+
+    // iterate through all super faces
+    priorLOD.faces.forEach( (face) => {
+      // bridge between the faultlinks on this super face
+    });
+
+    // update all links
+    this.links.forEach(link => link.updateDrawType());
+  }
+
+  // called if no priorLOD is passed to the constructor
+  // code in this function will be called in the super.constructor prior to mesh generation
+  protected _generate(): void {
+    // generate an icosahedron
+    super._generate();
+
+    // initialize unpicked faces after generating mesh
+    this.unpickedFaces = this.faces.slice();
+    this.linkedEdges = new Array<Edge>();
+
+    // add starting nodes until we've reached startingNodeDensity
+    while (this.links.length < (this.faces.length * this.startingNodeDensity)) {
+      // pick a random face
+      let pick = Math.floor(this.rng() * this.unpickedFaces.length);
+
+      // create a new FaultLink at that face
+      this.links.push(new FaultLink(this.unpickedFaces[pick]));
+      // remove the selected face so we don't pick it again
+      this.unpickedFaces.splice(pick, 1);
+    }
+
+    // iterate through initial nodes and ensure all adjacent faces have links
+    this.links.forEach( (link) => {
+      // iterate through adjacent faces
+      link.location.getAdjacentFaces().forEach( (face) => {
+	// find the face in unpickedFaces, if not found, unpickedIndex = -1
+	let unpickedIndex = this.unpickedFaces.indexOf(face);
+	if (unpickedIndex === -1) {
+	  // face has already been picked, so link its node to this node
+	  this.links.forEach( (otherLink) => { if (otherLink.location == face) otherLink.linkTo(link); });
+	} else {
+	  // face hasn't been picked so create a link there and link to it
+	  let newLink = new FaultLink(this.unpickedFaces[unpickedIndex]);
+	  this.links.push(newLink);
+	  this._connectLinks(newLink, link);
+	  // and remove it from unpickedFaces
+	  this.unpickedFaces.splice(unpickedIndex, 1);
+	}
+      });
+    });
+    
+    // iterate through all links and ensure they all have at least 2 connections
+    this._expandAllLinks();
+    
+    // update all links
+    this.links.forEach(link => link.updateDrawType());
+  }
+
+  protected _connectLinks(link0: FaultLink, link1: FaultLink): void {
+    // connect the links
+    link0.linkTo(link1);
+    // add the edge between them to linkedEdges
+    link0.location.edges.forEach( (edge) => {
+      if (edge.getFaceAdjacentTo(link0.location) == link1.location) {
+	this.linkedEdges.push(edge);
+      }
+    });
+    link1.location.edges.forEach( (edge) => {
+      if (edge.getFaceAdjacentTo(link1.location) == link0.location) {
+	this.linkedEdges.push(edge);
+      }
+    });
+  }
+
+  // iterate through all links and ensure they all have at least 2 connections
+  protected _expandAllLinks(): void {
+    for (let link of this.links) {
+      if (link.connections.size < 2) {
+	// get adjacent links to this link
+	let possibleConnectionFaces = new Set<Face>(link.location.getAdjacentFaces());
+	// remove the existing connection from possibleConnections
+	let existingConnection: FaultLink = link.connections.values().next().value;
+	possibleConnectionFaces.delete(existingConnection.location);
+	// pick a random connection to link to
+	let targetFace: Face = Array.from(possibleConnectionFaces)[Math.round(this.rng())];
+
+	// find the face in unpickedFaces, if not found, unpickedIndex = -1
+	let unpickedIndex: number = this.unpickedFaces.indexOf(targetFace);
+	if (unpickedIndex === -1) {
+	  // face has already been picked, so link its link to this link
+	  this.links.forEach( (otherLink) => { if (otherLink.location == targetFace) this._connectLinks(otherLink, link); });
+	} else {
+	  // create a new FaultLink at targetFace
+	  let newLink = new FaultLink(targetFace);
+	  this.links.push(newLink);
+	  this._connectLinks(newLink, link);
+	  // and remove it from unpickedFaces
+	  this.unpickedFaces.splice(unpickedIndex, 1);
+	}
+      }
+    }
+  }
+};
+
+export { TectonicLOD };
