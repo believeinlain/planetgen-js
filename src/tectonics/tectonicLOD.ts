6a7b8c35b0c4dd635e8adde81ba062c5ae3f3fe6
Stephan Aelmore <saelmore@connectiveunconscious.com>
2019-12-01 01:52:04 +0000

    split off faultlink into separate file

--- src/tectonics/tectonicLOD.ts
+++ src/tectonics/tectonicLOD.ts
@@ -1,103 +1,10 @@
 
 import { Point, Edge, Face } from '../geometry/primitives';
+import { FaultLink } from '../tectonics/faultLink';
 import { SphereLOD } from '../geometry/sphereLOD';
 
 // allow for seeded rng
 var seedrandom = require('seedrandom');
-
-// links between two other FaultLinks to form a faultline
-class FaultLink {
-  location: Face;
-  connections: Set<FaultLink>;
-
-  constructor (newLocation: Face) {
-    this.location = newLocation;
-    this.connections = new Set<FaultLink>();
-  }
-
-  // update the DrawType on this.location
-  updateDrawType(): void {
-    // find the edge indices on locations of this link's connections
-    let topUV;
-    let leftUV;
-    let rightUV;
-
-    // set uvs based on number and orientation of connections
-    switch(this.connections.size) {
-      case 3: // use the 'node' uvs
-	topUV = { u: 0.25, v: 1 };
-	leftUV = { u: 0, v: 0.5 };
-	rightUV = { u: 0.5, v: 0.5 };
-	this.location.debugUVs = [topUV.u, topUV.v, leftUV.u, leftUV.v, rightUV.u, rightUV.v];
-	break;
-      case 2: // use the 'link' uvs
-	topUV = { u: 0.25, v: 0.5 };
-	leftUV = { u: 0, v: 0 };
-	rightUV = { u: 0.5, v: 0 };
-	// find which point index is shared between the edges crossed by the two connections
-	let edges = Array.from([...this.connections], (link) => {
-	  return this.getEdgeAcrossConnectionTo(link);
-	});
-	let sharedPointIndex;
-	if (edges.length == 2) {
-	  sharedPointIndex = this.location.points.indexOf(edges[0].getSharedPoint(edges[1]));
-	} else {
-	  // this should not happen
-	  console.log("Error: Some connection must not have an edge, unable to set debug UV");
-	  return;
-	}
-
-	// this really should never happen
-	if (sharedPointIndex == undefined) {
-	  console.log("Error: point shared by connections does not exist or is not on the FaultLink's face ...");
-	  console.log("... unable to set debug UVs");
-	}
-
-	// rotate the UVs so that topUV is at the sharedPointIndex
-	let swapUV;
-	switch (sharedPointIndex) {
-	  case 0: // no need to rotate
-	    break;
-	  case 1: // rotate so that topUV is at leftUV
-	    swapUV = leftUV;
-	    leftUV = topUV;
-	    topUV = rightUV;
-	    rightUV = swapUV;
-	    break;
-	  case 2: // rotate so that topUV is at rightUV
-	    swapUV = rightUV;
-	    rightUV = topUV;
-	    topUV = leftUV;
-	    leftUV = swapUV;
-	    break;
-	  default: // this should also never happen
-	    console.log("Somehow the sharedPointIndex is > 2. what.");
-	}
-
-	// set uvs to the proper rotated uvs
-	this.location.debugUVs = [topUV.u, topUV.v, leftUV.u, leftUV.v, rightUV.u, rightUV.v];
-	break;
-      default: // this should never happen
-	console.log("Error: FaultLink detected with "+this.connections.size+" connections. Should be 3 or 2.");
-    }
-  }
-
-  // connect this link to another link
-  linkTo(link: FaultLink): void {
-    this.connections.add(link);
-    link.connections.add(this);
-  }
-
-  // get the edge between FaultLinks
-  getEdgeAcrossConnectionTo(other: FaultLink): Edge {
-    for (let edge of this.location.edges) {
-      if (edge.getFaceAdjacentTo(this.location) == other.location) {
-	return edge;
-      }
-    }
-    return undefined;
-  }
-};
 
 class TectonicLOD extends SphereLOD {
   startingNodeDensity: number; // ratio of faces that should be nodes
@@ -106,13 +13,13 @@ class TectonicLOD extends SphereLOD {
   unpickedFaces: Face[]; // all faces that don't have FaultLinks on them
   linkedEdges: Edge[]; // all between two FaultLinks
 
-  constructor (newRadius: number, pointsRef: Point[], options: any, priorLOD?: TectonicLOD) {
-    super(newRadius, pointsRef, options, priorLOD);
+  constructor (pointsRef: Point[], options: any, priorLOD?: TectonicLOD) {
+    super(pointsRef, options, priorLOD);
     // nothing goes here, we just override protected methods of sphereLOD instead
   }
 
-  protected _initialize(newRadius: number, pointsRef: Point[], options: any): void {
-    super._initialize(newRadius, pointsRef, options);
+  protected _initialize(pointsRef: Point[], options: any): void {
+    super._initialize(pointsRef, options);
     this.rng = seedrandom(options.seed);
     this.links = new Map<Face, FaultLink>();
     this.startingNodeDensity = options.nodeDensity;
